3383bb8901a01a74e2cde174231c64f7a7ba9184
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-06 20:27:37 +0530

    added Sidebar

---
+++ react-portfolio/src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.scss';
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/'>
+            <img src={LogoS} alt='logo' />
+            <img src={LogoSubtitle} alt='slobodan' />
+        </Link>
+    </div>
+)
+
+export default Sidebar
