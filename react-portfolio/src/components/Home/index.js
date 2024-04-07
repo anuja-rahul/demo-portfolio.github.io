070ef4d74fe5724b07bce1bc4170030ef721de84
Anuja Rahul <a.r.gunasinghe@gmail.com>
2024-04-07 16:02:53 +0530

    added page layout

---
+++ react-portfolio/src/components/Home/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss';
+
+const Home = () => {
+  return (
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
+          <img src={LogoTitle} alt="developer" />
+          lobodan
+          <br />
+          web developer
+        </h1>
+        <h2>
+            Frontend Developer
+        </h2>
+        <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+      </div>
+    </div>
+  );
+}
+export default Home
--- react-portfolio/src/components/Layout/index.js
+++ react-portfolio/src/components/Layout/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import Sidebar from '../Sidebar';
-import './index.scss';
+import React from 'react'
+import Sidebar from '../Sidebar'
+import './index.scss'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
-    return (
-        <>
-            <Sidebar />
-        </>
-    )
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;/body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;/html&gt;</span>
+        </span>
+      </div>
+    </div>
+  )
 }
 
 export default Layout
